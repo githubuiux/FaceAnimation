930c995146955d5466b48b1eee93b194f39481eb
githubuiux <uiuxista@gmail.com>
2023-07-09 12:07:20 +0300

    Initial Commit

---
+++ FaceAnimation/FaceAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAnimationApp.swift
+//  FaceAnimation
+//
+//  Created by Levent on 9.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
